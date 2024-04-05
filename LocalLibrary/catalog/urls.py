e0b962f817a608c6f34b129e62f04f74f28d7c6c
Jeffrey Anderson <jeffreyanderson392@gmail.com>
2024-04-04 20:47:55 -0500

    added part 9, forms and views and templates and a lot

--- LocalLibrary/catalog/urls.py
+++ LocalLibrary/catalog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,5 +10,24 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>',
          views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+
+urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  
+]
+
+
+# Add URLConf for librarian to renew a book.
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+
+# Add URLConf to create, update, and delete authors
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
